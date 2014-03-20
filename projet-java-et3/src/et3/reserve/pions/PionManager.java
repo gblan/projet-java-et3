5fd15c3ee0971df7f25a446a3bc656d307b2a4d4
guillaume.blanchard37 <guillaume.blanchard37@gmail.com>
2014-03-20 07:33:52 +0000

    déploiement des pions

--- projet-java-et3/src/et3/reserve/pions/PionManager.java
+++ projet-java-et3/src/et3/reserve/pions/PionManager.java
@@ -2,6 +2,52 @@ package et3.reserve.pions;
 
 public class PionManager {
 
-	/* Une méthode par type de déploiement*/
-	/* chaque pion apelle 1 ou plusieurs méthodes de déploiement*/
+	/* Une méthode par type de déploiement */
+	/* chaque pion apelle 1 ou plusieurs méthodes de déploiement */
+
+	public void deploiementHorizontal() {
+
+	}
+
+	public void deploiementBiaisGauche() {
+
+	}
+
+	public void deploiementBiaisDroit() {
+
+	}
+
+	public void contamination(Pion p) {
+		switch (p.getTypePion()) {
+		case TYPE1:
+			deploiementHorizontal();			
+			break;
+		case TYPE2:
+			deploiementBiaisDroit();			
+			break;
+		case TYPE3:
+			deploiementBiaisGauche();			
+			break;
+		case TYPE4:
+			deploiementBiaisGauche();
+			deploiementHorizontal();			
+			break;
+		case TYPE5:
+			deploiementBiaisDroit();
+			deploiementHorizontal();	
+			break;
+		case TYPE6:
+			deploiementBiaisDroit();
+			deploiementBiaisGauche();
+			break;	
+		case TYPE7:
+			deploiementHorizontal();
+			deploiementBiaisDroit();
+			deploiementBiaisGauche();
+			break;	
+		default:
+			break;
+		}
+
+	}
 }
