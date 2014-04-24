88dfe62f8a320b8c43f8d21e2798e572aa30052e
guillaume.blanchard37 <guillaume.blanchard37@gmail.com>
2014-04-24 13:43:31 +0000

    choix des niveaux fonctionnel.

--- projet-java-et3/src/et3/menus/Niveaux.java
+++ projet-java-et3/src/et3/menus/Niveaux.java
@@ -1,12 +1,18 @@
 package et3.menus;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.beans.binding.ListBinding;
+
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
+
+import main.Principale;
 
 import et3.grille.Grille;
 import et3.jeu.Jeu;
@@ -19,6 +25,7 @@ public class Niveaux extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private List<Jeu> listeNiveau;
+	private String[] listNiveau;
 
 	/**
 	 * @return Constructeur de niveaux
@@ -39,17 +46,28 @@ public class Niveaux extends JFrame {
 		setBounds(100, 100, 315, 454);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JList<Jeu> list = new JList<Jeu>();
-		JScrollPane listScroller = new JScrollPane(list);
+		JList list = new JList(listNiveau);
+		
+		list.setLayoutOrientation(JList.VERTICAL);
+		list.addMouseListener(new MouseAdapter() {
+		    public void mouseClicked(MouseEvent evt) {
+		        JList list = (JList)evt.getSource();
+		        if (evt.getClickCount() == 2) {
+		            int index = list.locationToIndex(evt.getPoint());
+		            System.out.println(index);
+					Principale p1 = new Principale("Sporos : niveau "+index,index, 300, 500);
 
-		for (Jeu j : this.listeNiveau) {
-			list.add("level : " + i, j);
-			i++;
-		}
+		        }
+		    }
+		});
+		
+		JScrollPane listScroller = new JScrollPane(list);
+		
 		getContentPane().add(listScroller);
 	}
 
 	private void buildLevels() {
+		String[] tmp = new String[100];
 		for (int i = 1; i < 100; i++) {
 			String str = "level" + i + ".properties";
 			File properties = new File(str);
@@ -57,8 +75,11 @@ public class Niveaux extends JFrame {
 				Jeu jeu = new Jeu(Grille.buildGrid(str),
 						Reserve.buildReserve(str));
 				this.listeNiveau.add(jeu);
+				tmp[i] = str;
 			}
 		}
+		listNiveau = tmp;
+
 	}
 
 	@Override
